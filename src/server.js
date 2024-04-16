639e863cf415834639b8bc8d58855472f9ad3350
Pedro Cavalheiro <pedro@cavalheiro.eu>
2024-04-16 19:21:03 +0000

    Add basic server setup with security and logging configured

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const logger = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
